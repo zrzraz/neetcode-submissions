f6ea4e9ad3595022e8c97a82e0e74230b29d9712
Zara R <181423993+zrzraz@users.noreply.github.com>
2026-05-11 22:24:15 -0400

    Add: missing-number - submission-2

---
+++ Data Structures & Algorithms/missing-number/submission-2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+
+        //given an array of nums
+        //containing integers in the range 0-n; n being the last index + 1
+        //return the single number in the range that is missing
+        sort(nums.begin(),nums.end());
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i]!= i) {
+                return i;
+            }
+        }
+        //if no mismatch is found
+
+        return nums.size();
+        
+    }
+};
